da3db907b0eb30b82807ec74d07fa83b11dfc9f7
Wes <wal2.designer@gmail.com>
2022-05-25 17:06:43 +0100

    wip: start treat samaller

--- quick_sort.py
+++ quick_sort.py
@@ -27,28 +27,17 @@ def quicksort(array, pivot_number=-1, head_compare=0):
     if array[pivot_number] == array[head_compare]:
         print('case 4', abs(pivot_number), abs(head_compare))
 
-        #
+        array_smaller = array[:pivot_number]
+        array_bigger = array[pivot_number:]
 
         if abs(pivot_number) == abs(head_compare):
             print('case 4 first if')
-
-            print(array)
 
             return quicksort(array, -1, array.index(array[head_compare + 1]))
 
         if abs(pivot_number) != abs(head_compare):
             print('case 4 second if')
-            print(array)
-
-            # if pivot_number - 1 < len(array):
             return quicksort(array, pivot_number - 1, head_compare)
-
-    # if head_compare + 1 < len(array):
-    #     return quicksort(array, -1, head_compare)
-
-    # else:
-    #     print(array)
-    #     return 'saiu'
 
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
