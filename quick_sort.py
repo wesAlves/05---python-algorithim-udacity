6550edd6a6f554d96e26524f5243689104412ef8
Wes <wal2.designer@gmail.com>
2022-05-25 13:24:20 +0100

    wip: working in the second part yet

--- quick_sort.py
+++ quick_sort.py
@@ -6,12 +6,14 @@ Output a sorted list."""
 def quicksort(array, pivot_number=-1, head_compare=0):
 
     if array[pivot_number] > array[head_compare]:
+        print(pivot_number)
 
         head_compare += 1
 
         return quicksort(array, pivot_number, head_compare)
 
     if array[pivot_number] < array[head_compare]:
+        print(pivot_number)
 
         temp_element = array[head_compare]
 
@@ -23,8 +25,40 @@ def quicksort(array, pivot_number=-1, head_compare=0):
 
         return quicksort(array, pivot, head_compare)
 
-    else:
-        return array
+    if array[pivot_number] == array[head_compare]:
+        print(pivot_number)
+        print(array[pivot_number] == array[head_compare])
+
+        # go smaller first
+        array_smaller = array[0:pivot_number]
+
+        if array_smaller[-1] > array_smaller[0]:
+            print('array last is samller than fisrt')
+
+            pivot = array.index((array_smaller[-1]))
+            # print(pivot)
+
+            return quicksort(array, pivot, head_compare)
+
+        # if array_smaller[-1] == array_smaller[0]:
+        #     print('array last is not samller than fisrt')
+        #     # print(array[pivot_number] == array[head_compare])
+
+    return array
+
+    # return quicksort(array, array_smaller.index(array_smaller[-1]))
+
+    # array_bigger = array[pivot_number + 1: -1]
+
+    # print(array)
+    # return quicksort(array, head_compare=pivot_number + 1)
+
+    # print(array_bigger)
+
+    # return quicksort(array_smaller)
+
+    # else:
+    #     return 'banana'
 
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
