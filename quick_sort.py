d4e028e8f0a980c1a73e7733b506f1843f26a576
Wes <wal2.designer@gmail.com>
2022-05-25 12:42:47 +0100

    wip: tring to do the second step

--- quick_sort.py
+++ quick_sort.py
@@ -5,31 +5,11 @@ Output a sorted list."""
 
 def quicksort(array, pivot_number=-1, head_compare=0):
 
-    # pivot = array[pivot_number]
-
     if array[pivot_number] > array[head_compare]:
-        print('I pivot is bigger')
-        # print (pivot_number)
 
         head_compare += 1
-        print(head_compare)
 
         return quicksort(array, pivot_number, head_compare)
-
-        # if array[pivot_number] > array[head_compare]:
-        #     print ('seunda posição')
-
-        #     temp_element = array[head_compare]
-
-        #     array[head_compare] = array[pivot_number - 1]
-        #     array[pivot_number - 1] = array[pivot_number]
-        #     array[pivot_number] = temp_element
-
-        #     pivot = pivot_number -1
-        #     return quicksort(array, pivot, head_compare )
-        # else:
-        #     return array
-    #     return quicksort(array)
 
     if array[pivot_number] < array[head_compare]:
 
@@ -41,17 +21,8 @@ def quicksort(array, pivot_number=-1, head_compare=0):
 
         pivot = pivot_number - 1
 
-        print('I  pivot is less')
-        # print(head_compare)
-        # print (pivot)
-        # print (array)
-
-        # return (array)
         return quicksort(array, pivot, head_compare)
 
-    # if pivot == array[0]:
-    #     print ('I dono yet')
-    #     return array
     else:
         return array
 
